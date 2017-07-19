eaa888a1d2466a99803a652cf5f9c73b2d84a611
Flarp <flurpdadurp@gmail.com>
2017-07-19 11:00:11 -0400

    Added everything but searching

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ extern crate rocket_contrib;
 extern crate dotenv;
 extern crate serde;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 
 use rocket_contrib::{ Template, Json };
 use rocket::response::content::Html;
@@ -23,8 +24,9 @@ use diesel::Connection;
 use diesel::prelude::*;
 use dotenv::dotenv;
 use rocket::request::Form;
+use serde_json::json;
 
-type CustomErr = Custom<Html<&'static str>>;
+type CustomErr = Custom<Template>;
 type HtmlResponder = Result<Html<&'static str>, CustomErr>;
 type RedirectResponder = Result<Response<'static>, CustomErr>;
 
@@ -36,11 +38,15 @@ enum DIWKError {
 const SESSION: &'static str = concat!("<html>\n<body>\n", include_str!("session.html"));
 const INDEX: &'static str = include_str!("index.html");
 const CREATE: &'static str = include_str!("create.html");
-const SESSION_FAIL: &'static str = concat!("<html>\n<body>\n", "<p>That ID does not exist. Please try another one.<p>\n", include_str!("session.html"));
-const INTERNAL_SESSION_ERROR: &'static str = concat!("<html>\n<body>\n", "<p>There was an internal error in the server. Please try again later</p>", include_str!("session.html"));
+//const SESSION_FAIL: &'static str = concat!("<html>\n<body>\n", "<p>That ID does not exist. Please try another one.<p>\n", include_str!("session.html"));
+//const INTERNAL_SESSION_ERROR: &'static str = concat!("<html>\n<body>\n", "<p>There was an internal error in the server. Please try again later</p>", include_str!("session.html"));
 
 diesel::embed_migrations!("migrations");
 diesel::infer_schema!("dotenv:DATABASE_URL");
+
+fn return_error<T: std::fmt::Display>(string: T) -> Template {
+  Template::render("error", json!( { "error": format!("{}", string) } ))
+}
 
 fn get_chart_with_id(id: i32) -> Result<OpinionChartSQL, DIWKError> {
   let connection = start_connection();
@@ -81,10 +87,32 @@ fn redirect(link: String) -> Response<'static> {
   .finalize()
 }
 
-#[get("/play/<id>")]
+fn in_common(result: &OpinionSessionQuery, integer: i64) -> Result<Vec<String>, DIWKError> {
+  match get_chart_with_id(result.chart_id) {
+    Ok(data) => {
+      let mut answers: Vec<String> = Vec::new();
+      let mut mixed: i64 = (result.opinion & integer) ^ std::i64::MIN;
+      for x in data.opinions.iter().rev() {
+        if mixed & 1 == 1 {
+          answers.push(x.clone());
+        }
+        mixed >>= 1;
+      } 
+      Ok(answers)
+
+    },
+    Err(err) => Err(err)
+  }
+
+}
+
+#[get("/view/<id>")]
 fn started(id: i32) -> Option<Template> {
   match get_session(id) {
     Ok(result) => {
+      if result.done == true {
+        
+      }
       match get_chart_with_id(result.chart_id) {
         Ok(x) => Some(Template::render("play", &x)),
         Err(_) => None
@@ -115,46 +143,52 @@ fn started(id: i32) -> Option<Template> {
 
 //makeshift bitfield
 fn integerify(bools: Vec<bool>, length: usize) -> Option<i64> {
-  if length != bools.len() {
-    return None;
-  }
+  let mut real_length: usize = 0;
   let mut num: i64 = 0;
   for &x in bools.iter() {
     num <<= 1;
     if x == true {
+      real_length += 1;
       num |= 1;
     };
   };
-  Some(num | std::i64::MIN)
+  
+  if length >= real_length { Some(num | std::i64::MIN) } else { None }
 
 }
 
-#[post("/play/<id>", format="application/json", data="<checks>")]
-fn answer(checks: Json<Vec<bool>>, id: i32) -> HtmlResponder {
+#[post("/view/<id>", format="application/json", data="<checks>")]
+fn answer(checks: Json<Vec<bool>>, id: i32) -> Result<Template, Custom<Template>> {
   let inner = checks.into_inner();
   match get_session(id) {
     Err(error) => match error {
-      DIWKError::NotFound => Err(Custom(Status::NotFound, Html(SESSION_FAIL))),
-      DIWKError::DieselError(_) => Err(Custom(Status::InternalServerError, Html("<p>Internal server error</p>"))) 
+      DIWKError::NotFound => Err(Custom(Status::NotFound, return_error("There are no sessions that exist with that ID. Please try another one."))),
+      DIWKError::DieselError(x) => Err(Custom(Status::InternalServerError, return_error(x)))
     },
     Ok(result) => {
+      if result.done == true {
+        return Err(Custom(Status::BadRequest, return_error("This game has already finished.")));
+      };
       match integerify(inner, result.max_checks as usize) {
-        None => Err(Custom(Status::BadRequest, Html("<p>filler</p>"))),
+        None => Err(Custom(Status::BadRequest, return_error("You have selected over the maximum amount of checks. Please refresh and try again."))),
         Some(integer) => {
           let connection = start_connection();
           if result.opinion.signum() == -1 {
-            match diesel::delete(opinionsessions::table.filter(opinionsessions::columns::id.eq(result.id))).execute(&connection) {
-              Err(_) => Err(Custom(Status::InternalServerError, Html("<p>No Good</p>"))),
-              Ok(_) => {
-                println!("{}", (result.opinion & integer) ^ std::i64::MIN);
-                Ok(Html("<p>Guni gugu</p>"))
-                
+            match in_common(&result, integer) {
+              Ok(strings) => match diesel::update(opinionsessions::table.filter(opinionsessions::columns::id.eq(result.id))).set(opinionsessions::columns::done.eq(true)).get_result::<OpinionSessionQuery>(&connection) {
+                Ok(_) => Ok(Template::render("results", json!({ "answers": strings }))),
+                Err(x) => Err(Custom(Status::InternalServerError, return_error(x)))
+              },
+              Err(err) => match err {
+                DIWKError::DieselError(err) => Err(Custom(Status::InternalServerError, return_error(err))),
+                DIWKError::NotFound => Err(Custom(Status::NotFound, return_error("Not Found. You messed something up bad.")))
               }
             }
+                
           } else {
-            match diesel::update(opinionsessions::table.filter(opinionsessions::columns::id.eq(result.id))).set(opinionsessions::columns::opinion.eq(integer)).get_result::<OpinionSessionQuery>(&connection) {
-              Ok(_) => Ok(Html("<p>Ok</p>")),
-              Err(_) => Err(Custom(Status::InternalServerError, Html("<p>No Good</p>")))
+            match diesel::update(opinionsessions::table.filter(opinionsessions::columns::id.eq(result.id))).set((opinionsessions::columns::opinion.eq(integer))).get_result::<OpinionSessionQuery>(&connection) {
+              Ok(_) => Ok(Template::render("answered", &result)),
+              Err(x) => Err(Custom(Status::InternalServerError, return_error(x)))
             }
           } 
         }
@@ -181,39 +215,24 @@ fn actually_start_game(form: Form<OpinionSessionForm>) -> RedirectResponder {
     Ok(_) => {
       let connection = start_connection();
       match diesel::insert(&value).into(opinionsessions::table).get_result::<OpinionSessionQuery>(&connection) {
-        Ok(x) => Ok(redirect(format!("/play/{}", x.id))),
-        Err(_) => Err(Custom(Status::InternalServerError, Html(INTERNAL_SESSION_ERROR)))
+        Ok(x) => Ok(redirect(format!("/view/{}", x.id))),
+        Err(x) => Err(Custom(Status::InternalServerError, return_error(x)))
       } 
 
     },
-    Err(_) => Err(Custom(Status::UnprocessableEntity, Html(SESSION_FAIL)))
-    /*
-    Ok(x) => {
-      match x.get(0) {
-        Some(_) => {
-          let connection = start_connection();
-          match diesel::insert(&value).into(opinionsessions::table).get_result::<OpinionSessionQuery>(&connection) {
-            Ok(x) => Ok(redirect(format!("/play/{}", x.id))),
-            Err(_) => Err(Custom(Status::InternalServerError, Html(INTERNAL_SESSION_ERROR)))
-          } 
-        },
-        None => Err(Custom(Status::new(422, "Invalid Chart ID"), Html(SESSION_FAIL)))
-      }
-    },
-  Err(_) => Err(Custom(Status::InternalServerError, Html(INTERNAL_SESSION_ERROR)))
-  */
+    Err(_) => Err(Custom(Status::UnprocessableEntity, return_error("Not Found")))
   } 
 }
 
 
 #[post("/create", format="application/json", data="<upload>")]
-fn post_create(upload: Json<OpinionChartJSON>) -> RedirectResponder {
+fn post_create(upload: Json<OpinionChartJSON>) -> Result<Template, Custom<Template>> {
   let form = upload.into_inner();
   let connection = start_connection();
   println!("{:?}", form);
   match diesel::insert(&form).into(opinioncharts::table).get_result::<OpinionChartSQL>(&connection) {
-    Ok(_) => Ok(redirect(String::from("/session"))),
-    Err(_) => Err(Custom(Status::InternalServerError, Html(INTERNAL_SESSION_ERROR))) 
+    Ok(x) => Ok(Template::render("created", &x)),
+    Err(x) => Err(Custom(Status::InternalServerError, return_error(x))) 
   }
 
 }
@@ -247,16 +266,19 @@ struct OpinionSessionForm {
   max_checks: i16,
 }
 
-#[derive(Queryable, Serialize, AsChangeset)]
+#[derive(Debug, Queryable, Serialize, AsChangeset)]
 #[table_name="opinionsessions"]
 struct OpinionSessionQuery {
   id: i32,
   chart_id: i32,
   max_checks: i16,
   opinion: i64,
+  done: bool
 }
 
 fn main() {
+
+
     dotenv().ok();
     println!("{}", integerify(vec![true, false, true, true], 4).unwrap());
     rocket::ignite()
